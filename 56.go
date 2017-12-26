7acda435724a8a586d3bbeae1169c65cef6f2b54
JJTimmons <joshua.timmons1@gmail.com>
2017-12-25 20:35:48 -0500

    finished and solved 56

--- 56.go
+++ 56.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math/big"
+)
+
 /*
 Powerful digit sum
 Problem 56
@@ -9,5 +14,27 @@ A googol (10^100) is a massive number: one followed by one-hundred zeros;
 Considering natural numbers of the form, a^b, where a, b < 100, what is the maximum digital sum?
 */
 func main() {
-
+	curr := new(big.Int)
+	currMod := new(big.Int)
+	currMax := big.NewInt(0)
+	sum := new(big.Int)
+	for a := 1; a < 100; a++ {
+		for b := 1; b < 100; b++ {
+			// set the current exponent
+			curr.Exp(big.NewInt(int64(a)), big.NewInt(int64(b)), nil)
+			sum.Set(big.NewInt(0))
+			for {
+				newMod := new(big.Int)
+				curr, currMod = newMod.DivMod(curr, big.NewInt(10), currMod)
+				sum.Set(sum.Add(sum, currMod))
+				if curr.Cmp(big.NewInt(0)) == 0 {
+					break
+				}
+			}
+			if sum.Cmp(currMax) > 0 {
+				currMax.Set(sum)
+			}
+		}
+	}
+	fmt.Println(currMax)
 }
