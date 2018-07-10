f805982f402e3d91c86c42d2b1f2fd0d51c1b2de
JJTimmons <joshua.timmons1@gmail.com>
2018-07-09 22:52:17 -0400

    more fails on 351

--- 351_w.py
+++ 351_w.py
@@ -1,4 +1,5 @@
 import utils
+import time
 """
 A hexagonal orchard of order n is a triangular lattice made up of points within a regular hexagon with side n.
 The following is an example of a hexagonal orchard of order 5:
@@ -14,25 +15,59 @@ Find H(100 000 000).
 """
 
 
-def hex_orchard(length=5):
-    total_count = sum(range(1, length + 1))  # num of coordinates
+def hex_orchard(order=5):
+    t1 = time.time()
 
-    PRIME_LIST = utils.prime_sieve(total_count, as_list=True)
+    PRIME_FACTORS = utils.gen_prime_factor_map(order + 1)
 
-    hidden = length - 1
-    hidden_coors = set()
+    CACHE = {}
 
-    for p in PRIME_LIST:
-        for x in range(p, length, p):
-            for y in range(p, length - x + 1, p):
-                hidden_coors.add((x, y))
+    def phi(n):
+        m = n
+        for p in PRIME_FACTORS[m]:
+            m -= m / p
+        CACHE[n] = m
+        return m
 
-    return (length - 1 + len(hidden_coors)) * 6
+    # print(time.time() - t1)
+
+    t2 = time.time()
+
+    length = order
+    hidden = 0  # 1 hidden for all but one x = 0 position
+    cache = {}
+    for x in range(2, length):
+
+        # number of times it fits fully within height available
+        y = (length - x / 2)
+        folds = y / x
+        hidden += (x - phi(x)) * folds
+
+        # count up the remaining coordinates
+        left = y % x
+        left_set = set()
+        for p in PRIME_FACTORS[x]:
+            left_set.update(range(p, left + 1, p))
+        hidden += len(left_set)
+
+        # fold_text = "folds created: " + str((x - phi(x)) * folds)
+        # rem_text = "remainder " + str(left) + " created: " + str(left_hidden)
+        # print('{0}, {1}  ->  {2}   {3}'.format(x, y, fold_text, rem_text))
+
+        # nums_hidden = set()
+        # for p in PRIME_FACTORS[x]:
+        #     nums_hidden.update(range(p, length - x, p))
+        # hidden += len(nums_hidden)
+
+    # print(time.time() - t2)
+
+    print hidden * 6
+
+    return 6 * (2 * (order - 1) + 2 * hidden)
 
 
-assert sum(range(1, 5 + 1)) == 15
-print hex_orchard(5)
-print hex_orchard(10)
-print hex_orchard(1000)
-# assert hex_orchard(1000) == 1177848
-print hex_orchard(10**4)
+# print hex_orchard(5)
+# hex_orchard(10)
+# hex_orchard(10**4) # 0.48
+# hex_orchard(10**5)  # 45.1
+hex_orchard(10)
