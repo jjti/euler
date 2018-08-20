0bfe2f31aeaf5d438389cc6350cbf2a50e1c58ce
JJTimmons <joshua.timmons1@gmail.com>
2018-08-20 10:34:48 -0400

    done with 174 edits

--- Done/174.py
+++ Done/174.py
@@ -1,14 +1,3 @@
-"""
-We shall define a square lamina to be a square outline with a square "hole" so that the shape possesses vertical and horizontal symmetry.
-
-Given eight tiles it is possible to form a lamina in only one way: 3x3 square with a 1x1 hole in the middle.
-However, using thirty-two tiles it is possible to form two distinct laminae.
-
-If t represents the number of tiles used, we shall say that t = 8 is type L(1) and t = 32 is type L(2).
-Let N(n) be the number of t ≤ 1000000 such that t is type L(n); for example, N(15) = 832.
-What is ∑ N(n) for 1 ≤ n ≤ 10?
-"""
-
 limit = 10**6 + 1
 
 L_SIEVE = [0] * (limit + 1)
@@ -26,8 +15,4 @@ for rings in [[x for x in range(8, limit, 8)],
             else:
                 break
 
-total = 0
-for y in range(1, 11):
-    total += len([x for x in L_SIEVE if x == y])
-
-print total  # 1.6 seconds
+print len([x for x in L_SIEVE if x > 0 and x < 11])  # 1.53 seconds
